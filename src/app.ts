3e552f87985e843ea9a46fae8103c6fcc4028803
fumi <fumifumi7629@gmail.com>
2022-02-27 10:32:11 +0900

    View preparation

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,2 @@
-import { Table } from "./Model/Table";
-import { Player } from "./Model/Player";
-import { Card } from "./Model/Card";
-import { GameDecision } from "./Model/GameDecision";
-import { Deck } from "./Model/Deck";
 
 
